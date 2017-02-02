9af4419d6ae50fa5b40fe2e4a1fb689cb941e180
DeividVeloso <veloso.deivid@gmail.com>
2017-02-02 19:41:08 -0200

    add reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+   return [
+     {title: 'JavaScript Book'},
+     {title: 'Harry Bobo Livro'},
+     {title: 'Torre Preta Livro'},
+     {title: 'Ruby, Quem Gosta?'}
+   ]
+}
