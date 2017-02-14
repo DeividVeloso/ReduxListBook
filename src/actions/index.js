8150b4e5b5455f7884ef92808cd21e59eba31fc2
Deivid Veloso <veloso.deivid@gmail.com>
2017-02-14 20:50:33 -0300

    add action and container BookDetail

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+//SelectBook é uma ActionCreator, que precisa retornar uma ação
+//um objeto com um tipo de propriedade
 export function selectBook(book){
-    console.log('A book has been selected:', book.title);
+  return {
+  type: 'BOOK_SELECTED', //Sempre em caixa alta, essa é a descrição da ação
+  payload: book // é o pedaço de dado que descreve a ação
+  }
 }
+ 
